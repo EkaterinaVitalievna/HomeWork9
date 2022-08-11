b9dcd3a887b90a942a388cd15afffb9a22a435f4
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-08-11 19:39:05 +0300

    натуральные числа в промежутке

---
+++ 64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N.
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+void ArrNumbers()
+{
+    Console.Write("Введите первое число: ");
+    int M = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Натуральные числа от M до N: ");
+
+    if (M < N)
+    {
+        for (int count = M; count < N; count++)
+        {
+            Console.Write($"{M++} ");
+        }
+    }
+    else
+    {
+        for (int count = N; count < M; count++)
+        {
+            Console.Write($"{N++} ");
+        }
+    }
+}
+
+ArrNumbers();
