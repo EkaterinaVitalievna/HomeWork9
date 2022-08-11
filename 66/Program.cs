0548b8be2043b4b6f588f6eabf41934e26dd730f
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-08-11 19:47:17 +0300

    сумма чисел от m до n

---
+++ 66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N.
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+void SumArrNumbers()
+{
+    Console.Write("Введите первое натуральное число: ");
+    int M = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе натуральное число: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+
+    int sum = 0;
+    if (M < N)
+    {
+        for (int count = M; count <= N; count++)
+        {
+            sum += +count;
+        }
+    }
+    else {
+        for (int count = N; count <= M; count++)
+        {
+            sum += +count;
+        }
+    }
+     Console.Write($"Сумма числе от первого введённого числа до второго: {sum}");
+}
+
+SumArrNumbers();
